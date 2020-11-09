6eda82d9d43cdc7f869d2d1b0ea34c8fd91fe743
Josh Fullmer <josh.fullmer@keap.com>
2020-11-09 11:05:32 -0700

    initial commit

---
+++ src/db/index.ts
@@ -0,0 +1,6 @@
+import { Sequelize } from 'sequelize';
+
+// TODO add configuration for postgres DB
+const sequelize = new Sequelize();
+
+export { sequelize };
---
+++ src/db/models/User.ts
@@ -0,0 +1,22 @@
+import { DataTypes, Model } from "sequelize/types";
+
+import { sequelize } from '..';
+
+interface UserAttributes {
+  username: string;
+}
+
+interface UserInstance extends Model<UserAttributes>, UserAttributes {}
+
+const User = sequelize.define<UserInstance>('user', {
+  username: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+});
+
+export { User };
