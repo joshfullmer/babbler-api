645fd0d682adc132a1ca369c7bbaaf995c760112
Josh Fullmer <josh.fullmer@keap.com>
2020-11-28 22:50:18 -0700

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { sequelize } from './db';
-import { User } from './db/models';
+import { User } from './models/User';
 import { HttpStatus } from './types/http';
 import { ValidationErrorItem } from 'sequelize/types';
 import bcrypt from "bcrypt";
@@ -23,8 +23,6 @@ app.use(bodyParser.json());
 //Look into migrations - as alter: true can be destructive
 //https://sequelize.org/master/manual/migrations.html
 sequelize.sync().then( async () => {
-  // await User.create({username: 'tom'});
-
   app.listen(4000, () => {
     console.log('server is ready');
   });
@@ -44,6 +42,7 @@ app.post('/signup', async (req, res) => {
 
   const hash = await bcrypt.hashSync(password, 10);
 
+  // TODO add null checking for email and password.
   if(!username) {
     res.status(HttpStatus.BAD_REQUEST).json({ error: "Username is required." });
 
@@ -53,6 +52,7 @@ app.post('/signup', async (req, res) => {
   try {
     const user = await User.create({ username, email, password: hash }); //this is creating a whole new object, not pointing to the previous object
 
+    // TODO send back a JWT, indicating they have successfully been logged in as well
     res.status(HttpStatus.CREATED).json(user);
   } catch ({ errors }) {
     const errorList = errors.map((error: ValidationErrorItem) => {
@@ -81,7 +81,7 @@ app.post('/login', async (req, res) => {
 
   if(bcrypt.compareSync(password, existingHash)) {
     // Passwords match
-    //return JWT for future requests
+    // TODO return JWT for future requests
     res.status(HttpStatus.ACCEPTED).json({ jwt: "" });
   } else {
     // Passwords don't match
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,6 @@
 import { DataTypes, Model } from "sequelize";
 
-import { sequelize } from '..';
+import { sequelize } from '../db';
 
 interface UserAttributes {
   username: string;
